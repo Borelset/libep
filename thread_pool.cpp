8c780add19c057bd47dc44cdb201d72fb2b14ea7
Borelset <xdnzxy@gmail.com>
2017-04-30 19:07:02 -0700

    update thread_pool

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,62 +1,60 @@
 #include <iostream>
-#include <pthread.h>
+#include <signal.h>
 #include "thread_pool.h"
 
 using namespace std;
 
+
+thread_info::thread_info()
+{
+    task = nullptr;
+    arg = nullptr;
+    pthread_mutex_init(&mutex, nullptr);
+    pthread_mutex_lock(&mutex);
+}
+
+wait_task::wait_task(void *(*_task)(void *), void *_arg)
+{
+    task = _task;
+    arg = _arg;
+}
+
 thread_pool::thread_pool()
 {
-    pthread_mutex_init(&mutex, nullptr);
+    sem_init(&idle_sem, 0, MAX_THREAD);
+    sem_init(&wait_sem, 0, 0);
 
     for(int i=0;i<MAX_THREAD;i++)
     {
         init_arg[i].order = i;
         init_arg[i].pool = this;
         pthread_create(&init_arg[i].tid, nullptr, thread_loop, &init_arg[i]);
+        idle_list.push_back(&thread_list[i]);
     }
-
+    pthread_create(&sche_tid, nullptr, sche_loop, (void*)this);
 }
 
 thread_pool::~thread_pool()
 {
     for(int i=0;i<MAX_THREAD;i++)
     {
-        while(1)
-        {
-            pthread_mutex_lock(&mutex);
-            if(thread_list[i].tag == 0)
-            {
-                thread_list[i].task = thread_exit;
-                thread_list[i].tag = 1;
-                pthread_mutex_unlock(&mutex);
-                break;
-            }
-            pthread_mutex_unlock(&mutex);
-        }
+        thread_list[i].task = thread_exit;
+        pthread_mutex_unlock(&thread_list[i].mutex);
     }
 
     for(int i=0;i<MAX_THREAD;i++)
     {
-        pthread_join(init_arg[i].tid, NULL);
+        pthread_join(init_arg[i].tid, nullptr);
     }
 }
 
 int thread_pool::run(void* (*task)(void *), void *arg)
 {
-    for(int i=0;i<MAX_THREAD;i++)
-    {
-        pthread_mutex_lock(&mutex);
-        if(thread_list[i].tag == 0)
-        {
-            thread_list[i].task = task;
-            thread_list[i].arg = arg;
-            thread_list[i].tag = 1;
-            pthread_mutex_unlock(&mutex);
-            return 0;
-        }
-        pthread_mutex_unlock(&mutex);
-    }
-    return -1;
+    struct wait_task temp(task, arg);
+    wait_list.push_back(temp);
+    sem_post(&wait_sem);
+
+    return 0;
 }
 
 void *thread_loop(void * arg)
@@ -65,18 +63,37 @@ void *thread_loop(void * arg)
 
     while(1)
     {
-        if(thi->pool->thread_list[thi->order].tag == 0)
+        pthread_mutex_lock(&thi->pool->thread_list[thi->order].mutex);
+        if(thi->pool->thread_list[thi->order].task == nullptr)
         {
 
         }
         else
         {
             thi->pool->thread_list[thi->order].task(thi->pool->thread_list[thi->order].arg);
-            pthread_mutex_lock(&thi->pool->mutex);
             thi->pool->thread_list[thi->order].task = nullptr;
-            thi->pool->thread_list[thi->order].tag = 0;
-            pthread_mutex_unlock(&thi->pool->mutex);
+            thi->pool->thread_list[thi->order].arg = nullptr;
+            thi->pool->idle_list.push_back(&thi->pool->thread_list[thi->order]);
+            sem_post(&thi->pool->idle_sem);
         }
+    }
+}
+
+void *sche_loop(void * arg)
+{
+    thread_pool* th_p = (thread_pool*)arg;
+    while(1)
+    {
+        sem_wait(&th_p->wait_sem);
+        sem_wait(&th_p->idle_sem);
+
+        struct thread_info* ptr = th_p->idle_list.front();
+        struct wait_task temp = th_p->wait_list.front();
+        ptr->arg = temp.arg;
+        ptr->task = temp.task;
+        pthread_mutex_unlock(&ptr->mutex);
+        th_p->idle_list.pop_front();
+        th_p->wait_list.pop_front();
     }
 }
 
@@ -85,3 +102,4 @@ void *thread_exit(void *)
     pthread_exit(0);
     return nullptr;
 }
+
--- thread_pool.h
+++ thread_pool.h
@@ -2,15 +2,25 @@
 #define THREAD_POOL_H
 
 #include <pthread.h>
-#define MAX_THREAD 20
+#include <semaphore.h>
+#include <list>
+#define MAX_THREAD 10
 
 class thread_pool;
 
 struct thread_info
 {
-    int tag = 0;
-    void* (*task)(void* arg) = nullptr;
-    void* arg = nullptr;
+    pthread_mutex_t mutex;
+    void* (*task)(void* arg);
+    void* arg;
+    thread_info();
+};
+
+struct wait_task
+{
+    void* (*task)(void* arg);
+    void* arg;
+    wait_task(void* (*)(void* arg), void*);
 };
 
 struct thread_init
@@ -25,15 +35,21 @@ class thread_pool
 private:
     struct thread_info thread_list[MAX_THREAD];
     struct thread_init init_arg[MAX_THREAD];
-    pthread_mutex_t mutex;
+    std::list<struct thread_info*> idle_list;
+    std::list<struct wait_task> wait_list;
+    pthread_t sche_tid;
+    sem_t idle_sem;
+    sem_t wait_sem;
 public:
     thread_pool();
     ~thread_pool();
     int run(void* (*)(void*), void* arg);
     friend void* thread_loop(void*);
+    friend void* sche_loop(void*);
 };
 
 void* thread_loop(void*);
-void *thread_exit(void*);
+void* sche_loop(void*);
+void* thread_exit(void*);
 
 #endif // THREAD_POOL_H
